65481139d70f669d0f1d2316ba4b1919e8149a38
webSiteDev-NH <nonomurahiroki0807@gmail.com>
2022-10-11 15:10:49 +0900

    動的パラメーター取得

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Param } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
   // GET /cats で findAll が動作
-  @Get()
-  findAll(): string {
-    return 'All cats';
+  // @Get()
+  // findAll(): string {
+  //   return 'All cats';
+  // }
+
+  // GET /cats/:id で cat が動作
+  // 別記述法：cat(@Param('id') id): string {
+  @Get(':id')
+  cat(@Param('id') id): string {
+    return `cat ${id}`;
   }
 
   // POST /cats で create が動作
