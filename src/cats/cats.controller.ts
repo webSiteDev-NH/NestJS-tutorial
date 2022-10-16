8fc411a0aa9575e223de9b765c7386af6a98c142
webSiteDev-NH <nonomurahiroki0807@gmail.com>
2022-10-16 11:24:38 +0900

    @Redirect

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Param, Req } from '@nestjs/common';
+import { Controller, Get, Post, Param, Req, Redirect } from '@nestjs/common';
 import { Request } from 'express';
 
 @Controller('cats')
@@ -11,10 +11,10 @@ export class CatsController {
 
   // GET /cats/:id で cat が動作
   // 別記述法：cat(@Param('id') id): string {
-  @Get(':id')
-  cat(@Param('id') id): string {
-    return `cat ${id}`;
-  }
+  // @Get(':id')
+  // cat(@Param('id') id): string {
+  //   return `cat ${id}`;
+  // }
 
   // @Req() /cats/req/:id で getReq が動作
   @Get('req/:id')
@@ -27,6 +27,13 @@ export class CatsController {
   @Get('mine')
   myCat(): string {
     return 'path /mine My cat';
+  }
+
+  @Get('redirect')
+  // リダイレクト先
+  @Redirect('mine')
+  redirect() {
+    console.log('redirect');
   }
 
   // POST /cats で create が動作
