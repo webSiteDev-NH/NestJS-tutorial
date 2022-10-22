be45865dc07ed595b22713909c5be84597670865
webSiteDev-NH <nonomurahiroki0807@gmail.com>
2022-10-22 10:04:27 +0900

    Script追加

--- frontend/pages/posts/first-post.tsx
+++ frontend/pages/posts/first-post.tsx
@@ -1,12 +1,21 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import Script from 'next/script'
 
+// lazyOnload：遅延読み込みで表示速度向上
 export default function FirstPost() {
   return  (
     <>
       <Head>
         <title>First Post</title>
       </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
       <h1>First Post</h1>
       <h2>
         <Link href="/">
